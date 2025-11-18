5bdd928ef8b1d3607e511d928db1bdf499687f3e
PIGSY <pigsy8@gmail.com>
2025-11-18 17:17:52 +0100

    created urls.py and views

---
+++ smtpr_main/contactbook/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("contact/list", views.contact_list, name="contact_list"),
+    path("contact/create", views.contact_create, name="contact_create"),
+    path("contact/del/", views.contact_del, name="contact_del"),
+
+    path("label/list", views.label_list, name="label_list"),
+    path("label/create", views.label_create, name="label_create"),
+    path("label/del", views.label_del, name="label_del"),
+
+    path("contact/add_label", views.add_label, name="add_label"),
+    path("contact/remove_label", views.remove_label, name="remove_label"),
+
+    path("true_del", views.true_del, name="true_del"),
+]
--- smtpr_main/contactbook/views.py
+++ smtpr_main/contactbook/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+import json
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponse
+from django.views.decorators.http import require_http_methods
+from .models import Contact, Label
+
+
+def api_test_page(request):
+    return HttpResponse("ok")
+
+
+def parse_body(request):
+    try:
+        return json.loads(request.body.decode()) if request.body else {}
+    except json.JSONDecodeError:
+        return {}
+
+
+def contact_create(request):
+    return HttpResponse("ok")
+
+
+def contact_list(request):
+    return HttpResponse("ok")
+
+
+def contact_del(request):
+    return HttpResponse("ok")
+
+
+def label_create(request):
+    return HttpResponse("ok")
+
+
+def label_list(request):
+    return HttpResponse("ok")
+
+
+def label_del(request):
+    return HttpResponse("ok")
+
+
+def true_del(request):
+    return HttpResponse("OK")
+
+
+def add_label(request):
+    return HttpResponse("OK")
+
+
+def remove_label(request):
+    return HttpResponse("OK")
